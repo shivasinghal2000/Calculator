952f5ceb227d0f1b30d16f23bdb7884dc4214941
shivasinghal2000 <singhalshiva936@gmail.com>
2020-09-20 18:24:21 +0530

    Updates

---
+++ precache-manifest.aba536938364912bb67f5e40947bcbc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc11f060c94043db5386fca14014114",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "94a576e3699254d77afd",
+    "url": "/Calculator/static/css/main.98e3e91d.chunk.css"
+  },
+  {
+    "revision": "46514c20b3edc2e9cc5e",
+    "url": "/Calculator/static/js/2.4ae3a3f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.4ae3a3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a576e3699254d77afd",
+    "url": "/Calculator/static/js/main.f1b6e985.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
